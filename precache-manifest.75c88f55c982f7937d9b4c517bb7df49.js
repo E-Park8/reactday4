f802f1745168c8a5f426f6cfcf28a6feb3f337cb
Eric Park <epark0995@gmail.com>
2020-09-17 11:48:25 -0700

    Updates

---
+++ precache-manifest.75c88f55c982f7937d9b4c517bb7df49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7565e32846fb343121cbc7f2bfb19898",
+    "url": "/reactday4/index.html"
+  },
+  {
+    "revision": "28af92d9e372e40709dd",
+    "url": "/reactday4/static/js/2.f2775c0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactday4/static/js/2.f2775c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a85dda8ec466d45ecd",
+    "url": "/reactday4/static/js/main.295cf57a.chunk.js"
+  },
+  {
+    "revision": "746a3b3ac062e7a68741",
+    "url": "/reactday4/static/js/runtime-main.3aa92cfb.js"
+  }
+]);
